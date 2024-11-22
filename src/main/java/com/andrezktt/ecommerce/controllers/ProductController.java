45b81e4e66d63bb2ea783d3c890b0fad07f69681
Andre Zicatti <andrezicatti@gmail.com>
2024-11-22 16:13:03 -0300

    DELETE product (delete)

--- src/main/java/com/andrezktt/ecommerce/controllers/ProductController.java
+++ src/main/java/com/andrezktt/ecommerce/controllers/ProductController.java
@@ -39,4 +39,10 @@ public class ProductController {
     public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
         return ResponseEntity.ok(service.update(id, dto));
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
