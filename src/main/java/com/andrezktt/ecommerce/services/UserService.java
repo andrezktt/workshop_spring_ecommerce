8b5fac16159223ffe6b501a93f4ea5ee6ebb6dd6
Andre Zicatti <andrezicatti@gmail.com>
2024-12-18 16:16:49 -0300

    GET Logged User

--- src/main/java/com/andrezktt/ecommerce/services/UserService.java
+++ src/main/java/com/andrezktt/ecommerce/services/UserService.java
@@ -1,14 +1,19 @@
 package com.andrezktt.ecommerce.services;
 
+import com.andrezktt.ecommerce.dto.UserDTO;
 import com.andrezktt.ecommerce.entities.Role;
 import com.andrezktt.ecommerce.entities.User;
 import com.andrezktt.ecommerce.projections.UserDetailsProjection;
 import com.andrezktt.ecommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -34,5 +39,22 @@ public class UserService implements UserDetailsService {
         }
 
         return user;
+    }
+
+    protected User authenticated() {
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String username = jwtPrincipal.getClaim("username");
+            return repository.findByEmail(username).get();
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("Email not found!");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO getMe() {
+        User user = authenticated();
+        return new UserDTO(user);
     }
 }
