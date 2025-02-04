62b5f01a9b8a2c3938cf3dcb82dc42789c3265bc
andre_zktt <andrezicatti@gmail.com>
2025-02-04 18:06:25 -0300

    :test_tube: test: Integration Tests - UserController

---
+++ src/test/java/com/andrezktt/ecommerce/controllers/it/UserControllerIntegrationTests.java
@@ -0,0 +1,85 @@
+package com.andrezktt.ecommerce.controllers.it;
+
+import com.andrezktt.ecommerce.tests.TokenUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerIntegrationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TokenUtil tokenUtil;
+
+    private String adminUsername, adminPassword;
+    private String clientUsername, clientPassword;
+    private String adminToken, clientToken, invalidToken;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        adminUsername = "alex@gmail.com";
+        adminPassword = "123456";
+
+        clientUsername = "maria@gmail.com";
+        clientPassword = "123456";
+
+        adminToken = tokenUtil.obtainAccessToken(mockMvc, adminUsername, adminPassword);
+        clientToken = tokenUtil.obtainAccessToken(mockMvc, clientUsername, clientPassword);
+        invalidToken = adminToken + "000";
+    }
+
+    @Test
+    public void getMeShouldReturnUserDTOWhenLoggedAsAdmin() throws Exception {
+        ResultActions result = mockMvc
+                .perform(get("/users/me")
+                        .header("Authorization", "Bearer " + adminToken)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.id").value(2L));
+        result.andExpect(jsonPath("$.name").value("Alex Green"));
+        result.andExpect(jsonPath("$.email").value("alex@gmail.com"));
+        result.andExpect(jsonPath("$.phone").value("977777777"));
+        result.andExpect(jsonPath("$.birthDate").value("1987-12-13"));
+        result.andExpect(jsonPath("$.roles").exists());
+    }
+
+    @Test
+    public void getMeShouldReturnUserDTOWhenLoggedAsClient() throws Exception {
+        ResultActions result = mockMvc
+                .perform(get("/users/me")
+                        .header("Authorization", "Bearer " + clientToken)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.id").value(1L));
+        result.andExpect(jsonPath("$.name").value("Maria Brown"));
+        result.andExpect(jsonPath("$.email").value("maria@gmail.com"));
+        result.andExpect(jsonPath("$.phone").value("988888888"));
+        result.andExpect(jsonPath("$.birthDate").value("2001-07-25"));
+        result.andExpect(jsonPath("$.roles").exists());
+    }
+
+    @Test
+    public void getMeShouldReturnUnauthorizedWhenInvalidToken() throws Exception {
+        ResultActions result = mockMvc
+                .perform(get("/users/me")
+                        .header("Authorization", "Bearer " + invalidToken)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isUnauthorized());
+    }
+}
